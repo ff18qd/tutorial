e16ecf82d08f5a6488aa3fc72f04fb55186e27e0
Marc Aaron <marc.aaron.glasser@gmail.com>
2018-07-14 10:54:27 -0700

    Set Up CRA, Add Dependencies, and Clean Up CRA boilerplate

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+          <h1>Styled Components & Atomic Design</h1>
+      <div>
+    );
+  }
+}
